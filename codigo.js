ace02f33d6a6a1e03945d39bce94fbdf031deec3
Andres Sanchez <prueba@correo.com>
2023-10-18 11:03:03 -0500

    guardar

---
+++ codigo.js
@@ -0,0 +1,51 @@
+function aleatorio(min, max) {
+    return  Math.floor(Math.random() * (max - min + 1) + min)
+}
+function eleccion(jugada) {   
+    let resultado = ""   
+    if(jugada == 1) {
+        resultado = "Piedra ✊"
+    }else if(jugada == 2) {
+        resultado = "Papel 📄"
+    } else if(jugada == 3) {
+        resultado = "Tijeras ✂️"
+    } else {
+        resultado = "MAL ELEGIDO"
+    }
+    return resultado
+}
+
+
+// 1 es piedra, 2 es papel, 3 es tijeras
+let jugador = 0
+let pc = 0
+let triunfos = 0
+let perdidas = 0
+while(triunfos < 3 && perdidas < 3) {
+        pc = aleatorio(1,3)
+        jugador = prompt ("elige 1 para piedra, 2 para papel, 3 para tijeras")
+        //alert("elegiste " + jugador)
+    
+        alert("PC elige: " + eleccion(pc))
+        alert("tu eliges: " + eleccion(jugador))
+
+        //COMBATE
+        if (pc == jugador) {
+        alert("EMPATAOS")
+    }else if (jugador == 1 && pc == 3) {
+        alert ("GANASTE")
+        triunfos = triunfos + 1
+    }else if (jugador == 2 && pc == 1 ) {
+        alert("GANASTE")
+        triunfos = triunfos + 1
+    }else if (jugador == 3 && pc == 2 ) {
+        alert("GANASTE")
+        triunfos = triunfos + 1
+    }else {
+        alert ("PERDISTE")
+        perdidas = perdidas + 1
+    }
+
+}
+
+alert("ganaste " + triunfos + " veces. perdiste " + perdidas + " veces.")
